b71c3f7b9acd1c8811d8d4991c7ce9cc121643ab
Rodrigo M. <t0tti-man@hotmail.com>
2026-07-05 20:41:44 -0300

    "Creo el paquete config y dentro del mismo, la clase WebConfig.java"

---
+++ src/main/java/com/talentotech/tpecommerce/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.talentotech.tpecommerce.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+    
+}
