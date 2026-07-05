b92d6a29f045b4566661a63aeb987bd15ceb0f1e
Rodrigo M. <t0tti-man@hotmail.com>
2026-07-05 20:17:49 -0300

    "se eliminaron los try y catch"

--- src/main/java/com/talentotech/tpecommerce/controller/CategoriaController.java
+++ src/main/java/com/talentotech/tpecommerce/controller/CategoriaController.java
@@ -35,44 +35,26 @@ public class CategoriaController {
         
         @GetMapping("/{id}")
         public ResponseEntity<Categoria> obtenerCategoria (@PathVariable int id) {
-            try {
                 return ResponseEntity.ok(service.obtenerPorId(id));
-            } catch(CategoriaNoEncontradaException e) {
-                return ResponseEntity.notFound().build();
-            }
         }
         
         
         @PostMapping
         public ResponseEntity<Categoria> crearCategoria (@Valid @RequestBody Categoria nuevaCategoria) {
-            try {
-                Categoria creada = service.guardar(nuevaCategoria);
-                return ResponseEntity.status(HttpStatus.CREATED).body(creada);
-            } catch(Exception e) {
-                return ResponseEntity.badRequest().build();
-            }
+                return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(nuevaCategoria));
         }
         
         @PutMapping("/{id}")
         public ResponseEntity<Categoria> actualizar (@PathVariable int id, @Valid @RequestBody Categoria datos) {
-            try {
                 return ResponseEntity.ok(service.actualizar(id, datos));
-            } catch (CategoriaNoEncontradaException e) {
-                return ResponseEntity.notFound().build();
-            } catch (CategoriaNombreInvalidoException e) {
-                return ResponseEntity.badRequest().build();
-            }
         }
         
         
         @DeleteMapping("/{id}")
         public ResponseEntity<Void> eliminar(@PathVariable int id) {
-            try {
+
                 service.eliminar(id);
                 return ResponseEntity.ok().build();
-            } catch (CategoriaNoEncontradaException e) {
-                return ResponseEntity.notFound().build();
-            }
-    }
+        }
         
 }
