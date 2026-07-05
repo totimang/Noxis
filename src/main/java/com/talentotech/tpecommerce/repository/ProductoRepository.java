d19c395b18baa4e01f9e03ffd42ea6046c98db55
Rodrigo M. <t0tti-man@hotmail.com>
2026-07-05 19:52:42 -0300

    "Agregamos el findByCategoriaNombreContainingIgnoreCase"

--- src/main/java/com/talentotech/tpecommerce/repository/ProductoRepository.java
+++ src/main/java/com/talentotech/tpecommerce/repository/ProductoRepository.java
@@ -3,14 +3,13 @@ package com.talentotech.tpecommerce.repository;
 import com.talentotech.tpecommerce.model.Producto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
 
 public interface ProductoRepository extends JpaRepository <Producto, Integer> {
     
     List<Producto> findByNombreContaining (String nombre);
     
-    @Query("SELECT p FROM Producto p WHERE p.categoria = :nombreCategoria")
-    List<Producto> buscarPorCategoria (@Param("nombreCategoria") String nombreCategoria);
+    List<Producto> findByCategoriaNombreContainingIgnoreCase(String nombre);
     
 }
