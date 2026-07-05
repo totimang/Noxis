5ffb1fe1a256838e366217358b3669956f702fe5
Rodrigo M. <t0tti-man@hotmail.com>
2026-07-05 20:57:05 -0300

    "Se creo la clase CarritoNoEncontradoException.java"

---
+++ src/main/java/com/talentotech/tpecommerce/exception/CarritoNoEncontradoException.java
@@ -0,0 +1,10 @@
+package com.talentotech.tpecommerce.exception;
+
+
+public class CarritoNoEncontradoException extends RuntimeException {
+    
+    public CarritoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+    
+}
