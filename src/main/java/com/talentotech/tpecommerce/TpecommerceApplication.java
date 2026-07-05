54bcb64e499f9edb608e2b251f608ae3ee3a676e
Rodrigo M. <t0tti-man@hotmail.com>
2026-07-05 20:42:59 -0300

    "Actualizo mi datos dentro de CommandLineRunner"

--- src/main/java/com/talentotech/tpecommerce/TpecommerceApplication.java
+++ src/main/java/com/talentotech/tpecommerce/TpecommerceApplication.java
@@ -20,18 +20,29 @@ public class TpecommerceApplication {
 	}
         
         @Bean
-        CommandLineRunner cargarDatos(ProductoService productoService, CategoriaService categoriaService) {
+        CommandLineRunner cargarDatos(CategoriaService categoriaService, ProductoService productoService) {
             return args -> {
                 if (categoriaService.listarTodas().isEmpty()) {
-                    Categoria memorias = categoriaService.guardar(new Categoria("Memorias", "Memorias RAM"));
-                    Categoria almacenamiento = categoriaService.guardar(new Categoria("Almacenamiento", "Discos rigidos y solidos"));
-                    Categoria procesadores = categoriaService.guardar(new Categoria("Procesadores", "Procesadores Intel y AMD"));
-                                
-                    productoService.guardar(new Producto("16GB DDR5", 378693.41, 20, memorias));
-                    productoService.guardar(new Producto("HD HDD 1TB", 14600, 10, almacenamiento));
-                    productoService.guardar(new Producto("AMD RYZEN 7 9800X3D", 771916.97, 10, procesadores));
-                    productoService.guardar(new Producto("32GB DDR5", 700030.97, 10, memorias));
-                    productoService.guardar(new Producto("Intel i7 8700k", 871916.97, 10, procesadores));
+                    Categoria memorias = categoriaService.guardar(new Categoria(null, "Memorias", "Memorias RAM"));
+                    Categoria almacenamiento = categoriaService.guardar(new Categoria(null, "Almacenamiento", "Discos rigidos y solidos"));
+                    Categoria procesadores = categoriaService.guardar(new Categoria(null, "Procesadores", "Procesadores Intel y AMD"));
+                    
+                    productoService.guardar(new Producto(null,"16GB DDR5", 378693.41, 20,
+                    "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+                    memorias));
+                    
+                    productoService.guardar(new Producto(null,"HD HDD 1TB", 14600, 20,
+                    "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+                    almacenamiento));
+                    
+                    productoService.guardar(new Producto(null,"AMD RYZEN 7 9800X3D", 771916.97, 10,
+                    "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+                    procesadores));
+                    
+                    productoService.guardar(new Producto(null,"32GB DDR5", 700030.97, 10,
+                    "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+                    memorias));
+
                 }                
             };
         }
